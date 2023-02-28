9f2d0a14c672848682a46cfc0919cc5e72d4695a
wangyu <88275668+123ddads@users.noreply.github.com>
2023-02-28 21:46:33 +0800

    Update mutex.c
    更新互斥锁的实现，利用内核链表的数据结构来管理Mutex变量，实现了创建互斥锁变量，进入临界区，离开临界区，销毁互斥锁变量的相关函数。

--- Kernel/mutex.c
+++ Kernel/mutex.c
@@ -1,28 +1,120 @@
 
 #include "mutex.h"
+#include "memory.h"
+
+static List gMList = {0};
 
 void MutexModInit()
 {
+    List_Init(&gMList);
 }
 
 void MutexCallHandler(uint cmd, uint param)
 {
     if( cmd == 0 )
     {
-        PrintString("Mutex Create\n");
-        PrintIntHex(param);
-        PrintChar('\n');
+        uint* pRet = (uint*)param;
+        
+        *pRet = (uint)SysCreateMutex();
     }
     else if( cmd == 1 )
     {
-        PrintString("Mutex Enter\n");
+        SysEnterCritical((Mutex*)param);
     }
     else if( cmd == 2 )
     {
-        PrintString("Mutex Exit\n");
+        SysExitCritical((Mutex*)param);
     }
     else 
     {
-        PrintString("Mutex Destroy\n");
+        SysDestroyMutex((Mutex*)param);
     }
 }
+
+Mutex* SysCreateMutex()
+{
+    Mutex* ret = Malloc(sizeof(Mutex));
+    
+    if( ret )
+    {
+        ret->lock = 0;  
+        
+        List_Add(&gMList, (ListNode*)ret);
+    }
+    
+    PrintString("Mutex ID: ");
+    PrintIntHex(ret);
+    PrintChar('\n');
+    
+    return ret;
+}
+
+static uint IsMutexValid(Mutex* mutex)
+{
+    uint ret = 0;
+    ListNode* pos = NULL;
+    
+    List_ForEach(&gMList, pos)
+    {
+        if( IsEqual(pos, mutex) )
+        {
+            ret = 1;
+            break;
+        }
+    }
+    
+    return ret;
+}
+
+void SysDestroyMutex(Mutex* mutex)
+{
+    if( mutex )
+    {
+        ListNode* pos = NULL;
+    
+        List_ForEach(&gMList, pos)
+        {
+            if( IsEqual(pos, mutex) )
+            {
+                List_DelNode(pos);
+                
+                Free(pos);
+                
+                PrintString("Destroy Mutex: ");
+                PrintIntHex(pos);
+                
+                break;
+            }
+        }
+    }
+}
+
+void SysEnterCritical(Mutex* mutex)
+{
+    if( mutex && IsMutexValid(mutex) )
+    {
+        if( mutex->lock )
+        {
+            PrintString("Move current to waitting status.\n");
+        }
+        else
+        {
+            mutex->lock = 1;
+            
+            PrintString("Enter critical section, access critical resource.\n");
+        }
+    }
+}
+
+
+void SysExitCritical(Mutex* mutex)
+{
+    if( mutex && IsMutexValid(mutex) )
+    {
+        mutex->lock = 0;
+        
+        PrintString("Notify all task to run again, critical resource is available.\n");
+    }
+}
+
+
