a007fc2d6b4ed3d3f7c8c61e603934726ca750a3
wangyu <88275668+123ddads@users.noreply.github.com>
2023-03-04 14:25:38 +0800

    Update demo2.c
    在读写者问题中引入等待机制来保持任务的互斥访问临界资源。

--- App/demo2.c
+++ App/demo2.c
@@ -9,7 +9,7 @@ static uint g_mutex_read = 0;
 static char g_data = 'A';
 static int g_count = 0;
 
-void Reader()
+static void Reader()
 {
     static int next = 0;
     static int col = 14;
@@ -55,12 +55,9 @@ void Reader()
     }
 }
 
-void Writer()
+static void Writer()
 {
     int next = 0;
-    
-    g_mutex_write = CreateMutex(Normal);
-    g_mutex_read = CreateMutex(Strict);
     
     SetPrintPos(0, 12);
     
@@ -87,4 +84,36 @@ void Writer()
     g_data = 0;
     
     ExitCritical(g_mutex_write);
+}
+
+static void Initialize()
+{
+    g_mutex_write = CreateMutex(Normal);
+    g_mutex_read = CreateMutex(Strict);
+}
+
+static void Deinit()
+{
+    Wait("Writer");
+    Wait("ReaderA");
+    Wait("ReaderB");
+    Wait("ReaderC");
+    
+    SetPrintPos(0, 20);
+    PrintString(__FUNCTION__);
+    
+    DestroyMutex(g_mutex_write);
+    DestroyMutex(g_mutex_read);
+}
+
+void RunDemo2()
+{
+    Initialize();
+    
+    RegApp("Writer",  Writer, 255);
+    RegApp("ReaderA", Reader, 255);
+    RegApp("ReaderB", Reader, 255);
+    RegApp("ReaderC", Reader, 255);
+    
+    RegApp("Deinit", Deinit, 255);
 }
