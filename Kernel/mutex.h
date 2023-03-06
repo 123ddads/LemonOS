5589d46e4cbb1a7f7afa51bc9e08cca1b4449913
wangyu <88275668+123ddads@users.noreply.github.com>
2023-03-06 09:10:41 +0800

    Update mutex.h
    为每个互斥锁增加一个等待队列，这样就不用使用全局队列来管理不同争夺互斥锁的任务。

--- Kernel/mutex.h
+++ Kernel/mutex.h
@@ -3,19 +3,24 @@
 #define MUTEX_H
 
 #include "type.h"
-#include "list.h"
+#include "queue.h"
 
-typedef struct {
-    ListNode* head;
+enum
+{
+    Normal,
+    Strict
+};
+
+typedef struct 
+{
+    ListNode head;
+    Queue wait;
+    uint type;
     uint lock;
 } Mutex;
 
 void MutexModInit();
-void MutexCallHandler(uint cmd, uint param);
+void MutexCallHandler(uint cmd, uint param1, uint param2);
 
-Mutex* SysCreateMutex();
-void SysDestroyMutex(Mutex* mutex);
-void SysEnterCritical(Mutex* mutex);
-void SysExitCritical(Mutex* mutex);
 
 #endif
